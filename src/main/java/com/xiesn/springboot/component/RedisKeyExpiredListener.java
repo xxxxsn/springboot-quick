5d9645dfee57ba1dbfb35f7a955fb8cb57d0249f
xieshaonan <xiaoshaonan@jwx.com.cn>
2021-04-25 16:31:52 +0800

    updete logs level

---
+++ src/main/java/com/xiesn/springboot/component/RedisKeyExpiredListener.java
@@ -0,0 +1,28 @@
+package com.xiesn.springboot.component;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import net.dreamlu.mica.core.utils.StringPool;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisKeyExpiredEvent;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RedisKeyExpiredListener {
+
+    /**
+     * redis key 超时事件（监听）
+     * @param event
+     */
+    @Async
+    @EventListener(RedisKeyExpiredEvent.class)
+    public void redisKeyExpiredEvent(RedisKeyExpiredEvent<Object> event) {
+        String redisKey = new String(event.getId());
+        String keyspace = event.getKeyspace();
+        String cacheKey = StrUtil.join(StringPool.COLON,keyspace, redisKey);
+        log.info(cacheKey);
+
+    }
+}
